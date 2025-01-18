3ac581d55fcd84e9954c03cfa135fec7d434a081
Gukson <75785907+Gukson@users.noreply.github.com>
2025-01-18 15:14:04 +0100

    slash xD

--- SchoolDiaryApp/urls.py
+++ SchoolDiaryApp/urls.py
@@ -61,7 +61,7 @@ urlpatterns = [
     #Uczeń
     #przegldaj oceny
     path('students/grates/', get_student_grades, name='get_student_grades'),
-    path('students/classes', get_student_classes, name='get_student_classes'),
+    path('students/classes/', get_student_classes, name='get_student_classes'),
     #================================================
     path('parents/', manage_parents, name='manage_parents'),
     path('schools/',school_view,name='school_view'),
